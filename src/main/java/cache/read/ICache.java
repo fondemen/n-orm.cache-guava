022b606bf5260d30f59bf4294c20abf0e13105a5
armellewamba <armellewamba@localhost>
2013-05-14 14:42:53 +0200

    adding some methods

--- src/main/java/cache/read/ICache.java
+++ src/main/java/cache/read/ICache.java
@@ -4,11 +4,42 @@ public interface ICache {
 	/**
 	 * Defines what a cache is.
 	 */
+	/**
+	 * Check if an element exists in the cache using the key
+	 */
+	public boolean exists(String key) throws Exception;
+	/**
+	 * delete an element in the cache using the key
+	 */
+	public void delete(String key) throws Exception;
+	/**
+	 * To insert an element in the cache
+	 */
+	public void insert(Object o, Object obj) throws Exception;
 	
-	public String getKey() throws Exception;
+	/**
+	 * return the the approximate number of entries in the cache
+	 */
+	public long size() throws Exception;
+	/**
+	 * delete all the element in the cache
+	 */
+	public void deleteAll() throws Exception;
+	/**
+	 * returns the maximum number of elements that can contain cache
+	 */
 	public long getMaximunSize() throws Exception;
-	public void setMaximunSize(long size) throws Exception;
+	/**
+	 * Change the maximum of elements that can contain cache
+	 */
+	public void setMaximunSize(int size) throws Exception;
+	/**
+	 * returns the life of elements contained in the cache
+	 */
 	public long getTTL() throws Exception;
+	/**
+	 * for change the life of element in the cache
+	 */
 	public void setTTL(long TTL) throws Exception;
 
 }
