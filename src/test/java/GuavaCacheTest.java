78bcabbcf56e70f3f9abb0fa23f397f76c6814ca
armellewamba <armellewamba@localhost>
2013-05-17 16:11:07 +0200

    Starting implement the test

--- src/test/java/GuavaCacheTest.java
+++ src/test/java/GuavaCacheTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,32 +7,24 @@ import org.junit.Test;
 import com.googlecode.n_orm.cache.read.guava.GuavaCache;
 
 
-public class GuavaCacheTest extends TestCase {
-	
-	private static int MAX_SIZE;
-	private static long TTL;
-	private GuavaCache gc;
-	
+public class GuavaCacheTest {
+	private GuavaCache gc=new GuavaCache();
+
 	@Before
 	public void setUp() throws Exception {
-		super.setUp();
-		MAX_SIZE=10;
-		TTL=2;
 	}
-
 	@After
 	public void tearDown() throws Exception {
-		MAX_SIZE=0;
-		TTL=0;
 	}
-	public void testSize() throws Exception{
-		long s=0;
-		assertEquals(s, gc.size());
-		}
 
 	/*@Test
 	public void test() {
 		fail("Not yet implemented");
 	}*/
+	@Test
+	public void testGetSize() throws Exception{
+		long size=0;
+		assertEquals(size, gc.size());
+	}
 
 }
