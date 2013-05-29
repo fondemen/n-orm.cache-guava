486c2b13bb85813b0040f4849e1b3ef66b0172ee
armellewamba <armellewamba@localhost>
2013-05-29 21:23:04 +0200

    new modification

--- src/main/java/com/googlecode/n_orm/cache/read/guava/GuavaCache.java
+++ src/main/java/com/googlecode/n_orm/cache/read/guava/GuavaCache.java
@@ -4,11 +4,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import com.google.common.cache.*;
 import com.googlecode.n_orm.cf.ColumnFamily;
 import com.googlecode.n_orm.storeapi.MetaInformation;
@@ -25,6 +22,7 @@ public class GuavaCache implements ICache {
 
 	}
 	
+	@Override
 	public void delete(MetaInformation meta, String table, String key)
 			throws CacheException {
 		Collection<ColumnFamily<?>> cfs = meta.getElement().getColumnFamilies();
@@ -38,6 +36,7 @@ public class GuavaCache implements ICache {
 			}
 		}
 
+	@Override
 	public void insertFamilyData(MetaInformation meta, String table,
 			String key, String family, Map<String, byte[]> familyData)
 			throws CacheException {
@@ -45,6 +44,7 @@ public class GuavaCache implements ICache {
 		cache.put(myKey, Collections.unmodifiableMap(new TreeMap<String,byte[]>(familyData)));
 	}
 
+	@Override
 	public Map<String, byte[]> getFamilyData(MetaInformation meta,
 			String table, String key,String family) throws CacheException {
 		String myKey=table.concat(key).concat(family);
@@ -52,30 +52,37 @@ public class GuavaCache implements ICache {
 		
 	}
 
+	@Override
 	public long size() throws CacheException {
 		return cache.size();
 		
 	}
 
+	@Override
 	public void reset() throws CacheException {
 		cache.invalidateAll();
 	}
 
+	@Override
 	public long getMaximunSize() throws CacheException {
 		return MAX_SIZE;
 
 	}
+	@Override
 	public void setMaximunSize(int size) throws CacheException {
-		this.MAX_SIZE=size;
+		GuavaCache.MAX_SIZE=size;
 
 	}
+	@Override
 	public long getTTL() throws CacheException {
 		return TTL;
 	}
+	@Override
 	public void setTTL(long TTL) throws CacheException {
-		this.TTL=TTL;
+		GuavaCache.TTL=TTL;
 
 	}
+	@Override
 	public boolean existsData(MetaInformation meta,
 			String table, String key,String family) throws CacheException{
 		if(this.getFamilyData(meta, table, key, family)!=null){
