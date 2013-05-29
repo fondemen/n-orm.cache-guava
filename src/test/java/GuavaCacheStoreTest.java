64e3995d6720aa8a10bcd7bbdc108ca6eed76bd2
armellewamba <armellewamba@localhost>
2013-05-29 21:26:30 +0200

    test guavaCacheStore

--- src/test/java/GuavaCacheStoreTest.java
+++ src/test/java/GuavaCacheStoreTest.java
@@ -2,7 +2,9 @@ import static org.junit.Assert.*;
 import static org.easymock.EasyMock.*;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +13,9 @@ import com.googlecode.n_orm.cache.read.guava.CacheException;
 import com.googlecode.n_orm.cache.read.guava.GuavaCacheStore;
 import com.googlecode.n_orm.cache.read.guava.ICache;
 import com.googlecode.n_orm.conversion.ConversionTools;
+import com.googlecode.n_orm.storeapi.DefaultColumnFamilyData;
 import com.googlecode.n_orm.storeapi.MetaInformation;
+import com.googlecode.n_orm.storeapi.Row.ColumnFamilyData;
 import com.googlecode.n_orm.storeapi.Store;
 
 
@@ -51,6 +55,37 @@ public class GuavaCacheStoreTest {
 		replay(mockCache);
 		sut.exists(null, "table", "key", "family");
 		verify(mockCache);
+	}
+	@Test
+	public void testExists() throws CacheException{
+		DefaultColumnFamilyData dcfd=new DefaultColumnFamilyData();
+		//GuavaCacheStore gcs=new GuavaCacheStore(mockStore, mockCache);
+		Set<String> families=new HashSet<String>();
+		families.add("family");
+		families.add("f");
+		families.add("fam");
+		families.add("Toto");
+		families.add("Tata");
+		families.add("Titi");
+		Map<String, byte[]> value = new HashMap<String, byte[]>();
+		value.put("Toto", new byte[10]);
+		
+		expect(mockCache.getFamilyData(null, "table", "id", "family")).andReturn(value);
+		expect(mockCache.getFamilyData(null, "table", "id", "f")).andReturn(value);
+		expect(mockCache.getFamilyData(null, "table", "id", "fam")).andReturn(value);
+		
+		ColumnFamilyData v = new DefaultColumnFamilyData();
+		v.put("Toto", value);
+		v.put("Tata", value);
+		v.put("Titi", value);
+		expect(mockStore.get(null, "table", "id", families)).andReturn(v);
+		mockCache.insertFamilyData(null, "table", "id", "family", v.get("Toto"));
+		mockCache.insertFamilyData(null, "table", "id", "family", v.get("Tata"));
+		mockCache.insertFamilyData(null, "table", "id", "family", v.get("Titi"));
+		replay(mockStore,mockCache);
+		sut.get(null, "table", "id", families);
+		verify(mockStore,mockCache);
+
 	}
 
 }
