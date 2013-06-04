06765b4de4c1e7c152f6a0bc5214a7df31f5d770
armellewamba <armellewamba@localhost>
2013-06-04 17:15:47 +0200

    test

---
+++ src/test/java/TestGuavaCache.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.n_orm.cache.read.guava.GuavaCache;
+
+
+public class TestGuavaCache {
+
+	@Before
+	public void setUp() throws Exception {
+		GuavaCache gc=new GuavaCache();
+	}
+	@Test
+	public void test(){
+		
+	}
+
+	
+
+}
